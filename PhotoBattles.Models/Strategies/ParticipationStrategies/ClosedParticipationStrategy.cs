81498eae7b8c199194b4a06298042b071daecfbe
Lyubomir-Panov <lyubomir.panov@gmail.com>
2015-11-04 21:25:21 +0200

    Attempt at fixing some issues with the participation strategy. Attempt failed.

--- PhotoBattles.Models/Strategies/ParticipationStrategies/ClosedParticipationStrategy.cs
+++ PhotoBattles.Models/Strategies/ParticipationStrategies/ClosedParticipationStrategy.cs
@@ -16,10 +16,15 @@
 
         public bool CanParticipate(string userName)
         {
-            if (this.contest.RegisteredParticipants.Any(rp => rp.UserName == userName)
-                && this.contest.Participants.Any(p => p.UserName != userName))
+            if (this.contest.IsActive && this.contest.RegisteredParticipants.Any(rp => rp.UserName == userName))
             {
-                return true;
+                if (!this.contest.Participants.Any() || this.contest.Participants.All(p => p.UserName != userName))
+                {
+                    if (this.contest.IsActive)
+                    {
+                        return true;
+                    }
+                }
             }
 
             return false;
